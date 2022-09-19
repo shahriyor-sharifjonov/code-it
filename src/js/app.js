af72c1f79eaf5d19a16fa9f46df32060e052496e
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-09-19 21:03:02 +0500

    header

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,29 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+const gifs = document.querySelectorAll('.gif');
+gifs.forEach(gif => {
+    const source = gif.querySelector('source')
+    source.remove()
+})
+
+const headerButton = document.querySelector(".header__button");
+const headerMenu = document.querySelector(".header__menu");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+
+headerButton.onclick = menuToggle;
+
+window.onclick = (e) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
