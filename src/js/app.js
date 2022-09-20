220204488eca9c23c0fb7194d5911a1ac55c01af
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-09-20 14:54:27 +0500

    footer

--- src/js/app.js
+++ src/js/app.js
@@ -6,11 +6,13 @@ functions.isWebp();
 
 // const swiper = new Swiper();
 
-const gifs = document.querySelectorAll('.gif');
-gifs.forEach(gif => {
+if(document.querySelectorAll('.gif')){
+  const gifs = document.querySelectorAll('.gif');
+  gifs.forEach(gif => {
     const source = gif.querySelector('source')
     source.remove()
-})
+  })
+}
 
 const headerButton = document.querySelector(".header__button");
 const headerMenu = document.querySelector(".header__menu");
@@ -30,4 +32,39 @@ window.onclick = (e) => {
     !e.composedPath().includes(headerMenu)
   )
     menuToggle();
-};
+}
+
+const openPopup = (el) => {
+  const popup = document.querySelector(`#${el}`)
+  popup.classList.add('active')
+}
+
+const closePopup = () => {
+  const popups = document.querySelectorAll('.popup');
+  popups.forEach(popup => {
+    popup.classList.remove('active')
+  })
+}
+
+if(document.querySelectorAll('.close-popup')){
+  const btns = document.querySelectorAll('.close-popup');
+  btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      closePopup()
+    })
+  })
+}
+
+if(document.querySelectorAll('.form')){
+  const forms = document.querySelectorAll('.form');
+  forms.forEach(form => {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const inputs = form.querySelectorAll('input')
+      inputs.forEach(input => {
+        input.value = ''
+      })
+      openPopup('thank')
+    })
+  })
+}
